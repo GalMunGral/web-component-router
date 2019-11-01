f87668219bd7667ce3994b6ac249a5f310cd926c
Wenqi He <hewenqi96@gmail.com>
2019-11-01 02:58:06 -0400

    Start rewrite

--- Router.js
+++ Router.js
@@ -1,43 +1,69 @@
-export class BaseRouter extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-  }
-
-  static get observedAttributes() {
-    return [ 'path' ];
+class Router extends HTMLElement {
+  static _instance = null;
+  static _pendingResolvers = [];
+  static path = '/';
+  
+  static get instance() {
+    if (this._instance) {
+      return Promise.resolve(this._instance);
+    }
+    let promise = new Promise((resolve, reject) => {
+      this._pendingResolvers.push(resolve);
+    });
+    return promise;
   }
   
-  updateLocation(newPath) {
-    this.setAttribute('path', newPath);
+  static set instance(node) {
+    this._instance = node;
+    this._pendingResolvers.forEach((resolve) => {
+      resolve(node);
+    })
   }
 
-  connectedCallback() {
-    window.addEventListener('load', () => {
-      window.locationService.subscribe(this);
-    });
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'path') {
-      let url = newValue;
-      this.render(url);
+  static updatePath(path) {
+    this._path = path;
+    window.history.pushState(null, 'test', path);
+    if (this._instance) {
+      this._instance.load(path);
     }
   }
 
-  // To be implemented by subclasses
-  render(url) {
-    this.shadowRoot.innerHTML = url
-  }
-}
-
-export class RouterLink extends HTMLAnchorElement {
   constructor() {
     super();
-    this.onclick = e => {
-      e.preventDefault(); // Don't load new page
-      let url = this.getAttribute('href');
-      window.locationService.goto(url);
+    if (Router._instance) {
+      console.warn('You have multiple `router-root` instances. Only the first one will be used');
+      return;
+    }
+    this.attachShadow({ mode: 'open' });
+    Router._instance = this;
+    this.routes = new Map();
+    for (let element of this.children) {
+      let path = element.getAttribute('path');
+      this.routes.set(path, element);
+    }
+    Router.path = this.getAttribute('default');
+    this.load(Router.path);
+  }
+
+  load(path) {
+    if (this.routes.has(path)) {
+      let activeElement = this.routes.get(path);
+      for (let el of this.shadowRoot.children) {
+        this.shadowRoot.removeChild(el);
+      }
+      this.shadowRoot.append(activeElement);
     }
   }
 }
+customElements.define('router-switch', Router);
+
+class RouterLink extends HTMLElement {
+  constructor() {
+    super();
+    this.path = this.getAttribute('href');
+    this.onclick = () => {
+      Router.updatePath(this.path);
+    }    
+  }
+}
+customElements.define('router-link', RouterLink);
